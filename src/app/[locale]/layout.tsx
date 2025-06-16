096ac75dd12b220f33adffb2e5a067c7167405eb
msavchuk <savcukmihajlo@gmail.com>
2025-06-16 19:21:22 +0300

    chore: prettier formating

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,33 +1,31 @@
-import {NextIntlClientProvider, hasLocale} from 'next-intl'
-import {routing} from '@src/i18n/routing'
-import {notFound} from 'next/navigation';
+import '@src/app/globals.css'
+import { routing } from '@src/i18n/routing'
+import { NextIntlClientProvider, hasLocale } from 'next-intl'
 import { IBM_Plex_Mono } from 'next/font/google'
-import "@src/app/globals.css";
+import { notFound } from 'next/navigation'
 
 const ibmPlexMono = IBM_Plex_Mono({
-  subsets: ['cyrillic', 'latin'],
-  weight: ['400', '500', '600']
+    subsets: ['cyrillic', 'latin'],
+    weight: ['400', '500', '600'],
 })
 
 export default async function RootLayout({
-  children,
-  params,
+    children,
+    params,
 }: Readonly<{
-  children: React.ReactNode;
-  params: Promise<{locale: string}>;
+    children: React.ReactNode
+    params: Promise<{ locale: string }>
 }>) {
+    const { locale } = await params
+    if (!hasLocale(routing.locales, locale)) {
+        notFound()
+    }
 
-  const {locale} = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
-
-  return (
-    <html lang={locale} className={ibmPlexMono.className}>
-      <body
-      >
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang={locale} className={ibmPlexMono.className}>
+            <body>
+                <NextIntlClientProvider>{children}</NextIntlClientProvider>
+            </body>
+        </html>
+    )
 }
