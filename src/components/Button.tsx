19e96d46fe4943152049550d8fc768c796c2dc36
msavchuk <savcukmihajlo@gmail.com>
2025-06-19 20:45:11 +0300

    chore: create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+interface ButtonProps {
+    className?: string
+    onClick?: () => void
+    children?: React.ReactNode
+}
+
+function Button({ className, onClick, children }: ButtonProps) {
+    const classNames = [
+        'cursor-pointer',
+        'border',
+        'border-black',
+        'dark:border-white',
+        'py-1',
+        'px-2',
+        'hover:border-slate-500',
+        'hover:text-slate-500',
+        className,
+    ].join(' ')
+
+    return (
+        <button className={classNames} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
