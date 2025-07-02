34ceeeb7339aaea017d4fb39c3e8e97a922ade9f
msavchuk <savcukmihajlo@gmail.com>
2025-07-02 21:47:59 +0300

    chore: admin auth middleware

---
+++ src/auth.middleware.ts
@@ -0,0 +1,7 @@
+import authConfig from './auth.config'
+import NextAuth from 'next-auth'
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    session: { strategy: 'jwt' },
+})
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,24 @@
-import createMiddleware from "next-intl/middleware"
-import { routing } from "@src/i18n/routing"
+import { auth } from '@src/auth.middleware'
+import { routing } from '@src/i18n/routing'
+import createMiddleware from 'next-intl/middleware'
+import { NextResponse } from 'next/server'
 
-export default createMiddleware(routing)
+const i18nMiddleware = createMiddleware(routing)
+
+export default auth(async (req) => {
+    const { nextUrl } = req
+    const isLoggedIn = !!req.auth
+    const locales = ['en', 'uk']
+
+    const paths = locales.flatMap((locale) => '/' + locale + '/admin')
+
+    if (!isLoggedIn && paths.includes(nextUrl.pathname)) {
+        return new NextResponse(null, { status: 404 })
+    }
+
+    return i18nMiddleware(req)
+})
 
 export const config = {
-    matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
