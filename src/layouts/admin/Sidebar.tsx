c8cf7a8fc0939dbd76a53ad7350e0875a25034eb
msavchuk <savcukmihajlo@gmail.com>
2025-07-03 13:23:15 +0300

    feat: admin page sidebar

---
+++ src/layouts/admin/Sidebar.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+function Sidebar() {
+    const t = useTranslations('admin')
+    const router = usePathname()
+    const pathWithoutLocale = router.replace(/\/[a-z]{2}\//, '/')
+
+    const sidebarList = [
+        {
+            name: t('sidebar.dashboard'),
+            href: '/admin',
+        },
+        {
+            name: t('sidebar.writeNew'),
+            href: '/admin/writenew',
+        },
+        {
+            name: t('sidebar.articles'),
+            href: '/admin/articles',
+        },
+        {
+            name: t('sidebar.drafts'),
+            href: '/admin/drafts',
+        },
+        {
+            name: t('sidebar.articleIdeas'),
+            href: '/admin/articleideas',
+        },
+    ]
+
+    return (
+        <aside className="h-full">
+            <nav className="h-full">
+                <ul className="border flex flex-col gap-3 w-48 h-full py-4">
+                    {sidebarList.map((item, index) => (
+                        <Link href={item.href}>
+                            <li
+                                className={
+                                    'hover:bg-gray-400 hover:text-black py-2 px-2 ' +
+                                    (item.href === pathWithoutLocale
+                                        ? 'bg-gray-400 text-black'
+                                        : '')
+                                }
+                                key={index}
+                            >
+                                {item.name}
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar
