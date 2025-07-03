79f975892c7b0fd5d3067a6b179cd8a062ef95f1
msavchuk <savcukmihajlo@gmail.com>
2025-07-03 12:08:48 +0300

    chore: change padding

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,20 +1,21 @@
-import { useTranslations } from "next-intl"
-import Link from "next/link"
-import LocaleSwitcher from "@src/components/LocaleSwitcher"
-import ThemeSwitcher from "@src/components/ThemeSwitcher"
-import TorIcon from "@src/components/svg/header/TorIcon"
+import { auth } from '@src/auth'
+import LocaleSwitcher from '@src/components/LocaleSwitcher'
+import ThemeSwitcher from '@src/components/ThemeSwitcher'
+import TorIcon from '@src/components/svg/header/TorIcon'
+import { getTranslations } from 'next-intl/server'
+import Link from 'next/link'
 
-function Header() {
-    const t = useTranslations("common")
-    
+async function Header() {
+    const session = await auth()
+    const t = await getTranslations('common')
 
     return (
-        <header className="pt-5 dark:text-white">
+        <header className="py-5 dark:text-white">
             <div className="container">
                 <div className="flex justify-around px-6">
                     <span className="w-full text-xl">
                         <Link href="/" className="font-medium">
-                            {t("header.name")}
+                            {t('header.name')}
                         </Link>
                     </span>
                     <div className="flex gap-6">
@@ -22,7 +23,7 @@ function Header() {
                             <ul className="flex gap-5 cursor-pointer">
                                 <li>
                                     <Link href="/blog">
-                                        {t("header.nav.blog")}
+                                        {t('header.nav.blog')}
                                     </Link>
                                 </li>
                                 {/* <li>
@@ -40,13 +41,18 @@ function Header() {
                         <div>
                             <a
                                 href="http://msavchukgt7dzz7u3v7gkmrdgwdvuahurhti6sb3pu3rkwrndob6zcid.onion/"
-                                title={t("header.tor")}
+                                title={t('header.tor')}
                             >
                                 <TorIcon />
                             </a>
                         </div>
                         <LocaleSwitcher />
                         <ThemeSwitcher />
+                        {session ? (
+                            <Link href={'/admin'}>Admin</Link>
+                        ) : (
+                            <div></div>
+                        )}
                     </div>
                 </div>
             </div>
