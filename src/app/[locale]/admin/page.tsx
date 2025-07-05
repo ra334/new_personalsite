1b96facdfe297726c8749ab24ffeb13a02faf855
msavchuk <savcukmihajlo@gmail.com>
2025-07-05 18:51:10 +0300

    chore: make panel style

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,17 +1,17 @@
-import { auth } from '@src/auth'
 import Header from '@src/layouts/Header'
 import Sidebar from '@src/layouts/admin/Sidebar'
 
 async function AdminPage() {
-    const session = await auth()
-
     return (
-        <>
+        <div className="h-screen flex flex-col pb-3">
             <Header />
-            <div className="container h-full w-full border p-4">
+            <div className="border container p-2 mb-3 flex flex-1">
                 <Sidebar />
+                <main className="h-full w-full flex justify-center items-center">
+                    <h1>Dashboard page</h1>
+                </main>
             </div>
-        </>
+        </div>
     )
 }
 
