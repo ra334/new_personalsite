1cf92fa00802817302cc495734837aeb3200219e
msavchuk <savcukmihajlo@gmail.com>
2025-07-08 16:55:24 +0300

    chore: add mediaTypeEnum for media type, mime field, url field make unique

--- db/schema/medias-schema.ts
+++ db/schema/medias-schema.ts
@@ -1,10 +1,26 @@
-import { timestamp, pgTable, text, uuid, boolean } from 'drizzle-orm/pg-core'
+import {
+    timestamp,
+    pgTable,
+    text,
+    uuid,
+    boolean,
+    pgEnum,
+} from 'drizzle-orm/pg-core'
+
+export const mediaTypeEnum = pgEnum('media_type', [
+    'image',
+    'video',
+    'audio',
+    'document',
+    'archive',
+    'other',
+])
 
 export const medias = pgTable('medias', {
     id: uuid('id').primaryKey().defaultRandom(),
-    url: text('url').notNull(),
-    type: text('type').notNull(),
-    alt: text('alt'),
+    url: text('url').notNull().unique(),
+    mime: text('mime').notNull(),
+    type: mediaTypeEnum('type').notNull(),
     isTemp: boolean('isTemp').notNull().default(true),
     createdAt: timestamp('createdAt', { mode: 'date' }).notNull().defaultNow(),
 })
