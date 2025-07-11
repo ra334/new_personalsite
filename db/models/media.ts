ca7ac7a0140307f7984011e3bf515d1bea87acb9
msavchuk <savcukmihajlo@gmail.com>
2025-07-11 21:17:02 +0300

    refactoring: one function for update value

--- db/models/media.ts
+++ db/models/media.ts
@@ -1,6 +1,6 @@
 import { db } from '../postgress'
 import { medias } from '../schema/medias'
-import { eq } from 'drizzle-orm'
+import { eq, count } from 'drizzle-orm'
 
 export type MediaType =
     | 'image'
@@ -17,6 +17,13 @@ export interface Media {
     type: MediaType
     isTemp: boolean
     createdAt: Date
+}
+
+export interface UpdateMediaData {
+    url?: string
+    mime?: string
+    type?: MediaType
+    isTemp?: boolean
 }
 
 export async function createMedia(
@@ -54,67 +61,30 @@ export async function getMediaById(id: string): Promise<Media> {
     return media
 }
 
-export async function updateMediaUrl(id: string, url: string) {
+export async function getTempMedia(): Promise<Media[]> {
+    const mediaList = await db.query.medias.findMany({
+        where: eq(medias.isTemp, true),
+    })
+
+    if (!mediaList.length) {
+        throw new Error('No temporary media found')
+    }
+
+    return mediaList
+}
+
+export async function updateMedia(
+    id: string,
+    data: UpdateMediaData,
+): Promise<Media> {
     const media = await db
         .update(medias)
-        .set({ url })
+        .set(data)
         .where(eq(medias.id, id))
         .returning()
 
     if (!media.length) {
         throw new Error('Media not found or update failed')
-    }
-
-    return media[0]
-}
-
-export async function updateMediaMime(id: string, mime: string) {
-    const media = await db
-        .update(medias)
-        .set({ mime })
-        .where(eq(medias.id, id))
-        .returning()
-
-    if (!media.length) {
-        throw new Error('Media not found or update failed')
-    }
-
-    return media[0]
-}
-
-export async function updateMediaType(id: string, type: MediaType) {
-    const media = await db
-        .update(medias)
-        .set({ type })
-        .where(eq(medias.id, id))
-        .returning()
-
-    if (!media.length) {
-        throw new Error('Media not found or update failed')
-    }
-
-    return media[0]
-}
-
-export async function updateMediaIsTemp(id: string, isTemp: boolean) {
-    const media = await db
-        .update(medias)
-        .set({ isTemp })
-        .where(eq(medias.id, id))
-        .returning()
-
-    if (!media.length) {
-        throw new Error('Media not found or update failed')
-    }
-
-    return media[0]
-}
-
-export async function deleteMedia(id: string) {
-    const media = await db.delete(medias).where(eq(medias.id, id)).returning()
-
-    if (!media.length) {
-        throw new Error('Media not found or delete failed')
     }
 
     return media[0]
@@ -135,4 +105,38 @@ export async function getAllMedia(
     }
 
     return mediaList
+}
+
+export async function countTempMedia(): Promise<number> {
+    const result = await db
+        .select({ value: count() })
+        .from(medias)
+        .where(eq(medias.isTemp, true))
+
+    return result[0].value
+}
+
+export async function countParmanentMedia(): Promise<number> {
+    const result = await db
+        .select({ value: count() })
+        .from(medias)
+        .where(eq(medias.isTemp, false))
+
+    return result[0].value
+}
+
+export async function countAllMedia(): Promise<number> {
+    const result = await db.select({ value: count() }).from(medias)
+
+    return result[0].value
+}
+
+export async function deleteMedia(id: string) {
+    const media = await db.delete(medias).where(eq(medias.id, id)).returning()
+
+    if (!media.length) {
+        throw new Error('Media not found or delete failed')
+    }
+
+    return media[0]
 }
