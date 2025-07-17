fcbef0a513e7349607dcff979bcd2f3bed1cc6bf
msavchuk <msavchuk.dev@gmail.com>
2025-07-17 18:31:30 +0300

    chore: get draft article images for admin

---
+++ src/app/api/blog/draft/[slug]/[media]/route.ts
@@ -0,0 +1,47 @@
+import { auth } from '@src/auth'
+import fs from 'fs'
+import { NextRequest } from 'next/server'
+import path from 'path'
+
+interface Params {
+    params: {
+        slug: string
+        media: string
+    }
+}
+
+export async function GET(req: NextRequest, { params }: Params) {
+    const session = await auth()
+
+    if (session) {
+        const { media, slug } = await params
+
+        const mediaPath = path.join(
+            process.cwd(),
+            'blog',
+            'drafts',
+            slug,
+            media,
+        )
+
+        if (!fs.existsSync(mediaPath)) {
+            return new Response(JSON.stringify({ error: 'File not found' }), {
+                status: 404,
+            })
+        }
+
+        const fileBuffer = fs.readFileSync(mediaPath)
+        const ext = path.extname(mediaPath).slice(1)
+
+        return new Response(fileBuffer, {
+            status: 200,
+            headers: {
+                'Countent-Type': `image/${ext}`,
+            },
+        })
+    } else {
+        return new Response(JSON.stringify({ message: 'Not authenticated' }), {
+            status: 401,
+        })
+    }
+}
