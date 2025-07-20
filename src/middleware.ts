6313b7ca9bc436edc1934a57d4a45e6ae2ab5e5e
msavchuk <msavchuk.dev@gmail.com>
2025-07-20 12:28:16 +0300

    fix: cloudflare 1000

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,26 @@
-import { auth } from '@src/auth.middleware'
 import { routing } from '@src/i18n/routing'
+import { getToken } from 'next-auth/jwt'
 import createMiddleware from 'next-intl/middleware'
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
 const i18nMiddleware = createMiddleware(routing)
 
-export default auth(async (req) => {
-    const { nextUrl } = req
-    const isLoggedIn = !!req.auth
-    const locales = ['en', 'uk']
+export default async function middleware(req: NextRequest) {
+    if (process.env.NEXTAUTH_URL_INTERNAL) {
+        process.env.NEXTAUTH_URL = process.env.NEXTAUTH_URL_INTERNAL
+    }
 
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+    const isLoggedIn = !!token
+    const locales = ['en', 'uk']
     const paths = locales.flatMap((locale) => '/' + locale + '/admin')
 
-    if (!isLoggedIn && paths.includes(nextUrl.pathname)) {
+    if (!isLoggedIn && paths.includes(req.nextUrl.pathname)) {
         return new NextResponse(null, { status: 404 })
     }
 
     return i18nMiddleware(req)
-})
+}
 
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
